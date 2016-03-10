8b9a678fa8cf0e124b187e525c22aa616cd182e3
huangziyi01 <huangziyi01@baidu.com>
2016-03-10 19:59:08 +0800

    quick push

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import RenderTo from 'fit-render-to'
 import './index.scss'
 
 export default class Modal extends React.Component {
@@ -39,44 +40,46 @@ export default class Modal extends React.Component {
         others.style.display = show ? 'block' : null
 
         return (
-            <div {...others} className={classes}
-                             onClick={this.handleOutClick.bind(this)}
-                             tabIndex="-1">
-                <div className="modal-dialog"
-                     onClick={this.handleModalClick.bind(this)}>
-                    <div className="modal-content">
-                        {title === '' ? null :
-                            <div className="modal-header">
-                                <button type="button"
-                                        className="close">
-                                    <span onClick={this.handleCancel.bind(this)}>×</span>
-                                    <span className="sr-only">Close</span>
-                                </button>
-                                <h4 className="modal-title">{title}</h4>
-                            </div>
-                        }
-                        <div className="modal-body"
-                             style={{marginTop:title===''?20:null}}>
-                            {children}
-                        </div>
-                        <div className="modal-footer">
-                            {renderOperateButton() ? renderOperateButton(this.handleOk.bind(this), this.handleCancel.bind(this)) :
-                                <div>
+            <RenderTo>
+                <div {...others} className={classes}
+                                 onClick={this.handleOutClick.bind(this)}
+                                 tabIndex="-1">
+                    <div className="modal-dialog"
+                         onClick={this.handleModalClick.bind(this)}>
+                        <div className="modal-content">
+                            {title === '' ? null :
+                                <div className="modal-header">
                                     <button type="button"
-                                            onClick={this.handleCancel.bind(this)}
-                                            className="btn btn-secondary">
-                                        {cancelText}
+                                            className="close">
+                                        <span onClick={this.handleCancel.bind(this)}>×</span>
+                                        <span className="sr-only">Close</span>
                                     </button>
-                                    <button type="button"
-                                            className="btn btn-primary"
-                                            onClick={this.handleOk.bind(this)}>{okText}</button>
+                                    <h4 className="modal-title">{title}</h4>
                                 </div>
                             }
-                        </div>
+                            <div className="modal-body"
+                                 style={{marginTop:title===''?20:null}}>
+                                {children}
+                            </div>
+                            <div className="modal-footer">
+                                {renderOperateButton() ? renderOperateButton(this.handleOk.bind(this), this.handleCancel.bind(this)) :
+                                    <div>
+                                        <button type="button"
+                                                onClick={this.handleCancel.bind(this)}
+                                                className="btn btn-secondary">
+                                            {cancelText}
+                                        </button>
+                                        <button type="button"
+                                                className="btn btn-primary"
+                                                onClick={this.handleOk.bind(this)}>{okText}</button>
+                                    </div>
+                                }
+                            </div>
 
+                        </div>
                     </div>
                 </div>
-            </div>
+            </RenderTo>
         )
     }
 }
